d81ebccee9e02a2e4778bd116196b18f219b120a
Valdeci <moura97.valdeci@gmail.com>
2021-08-13 12:53:26 -0300

    Cria pasta Fundamentos

--- README.md
+++
@@ -1,3 +0,0 @@
-Repositório com exercícios feitos por mim, Valdeci de Moura, enquanto aluno do curso desenvolvedor full-stack da Trybe.
-
-
--- bloco4-JS-Log-prog/dia4.1-introd-js/operadores.js
+++
@@ -1,8 +0,0 @@
-const a = 4;
-const b = 5;
-
-console.log('A soma dos valores é: ' + (a + b));
-console.log('A subtração dos valores é: ' + (a - b));
-console.log('A multiplicação dos valores é: ' + (a * b));
-console.log('A divisão dos valores é: ' + (a / b));
-console.log('O módulo dos valores é: ' + (a % b));
