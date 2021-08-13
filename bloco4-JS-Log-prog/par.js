7e4a352d827f18f95b16f1fc3f6548623121b61f
Valdeci <moura97.valdeci@gmail.com>
2021-08-12 21:22:05 -0300

    Exercício 9

--- bloco4-JS-Log-prog/par.js
+++ bloco4-JS-Log-prog/par.js
@@ -2,11 +2,11 @@ const a = 3;
 const b = 7;
 const c = 5;
 
-function oddTrue() {
+function evenTrue() {
   if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0) {
     return true;
   };
   return false;
 };
 
-console.log(oddTrue(a, b, c))
+console.log(evenTrue(a, b, c))
