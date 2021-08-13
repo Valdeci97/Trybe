879165388ee52ae8e75c01bef23c6309f9bcf0f8
Valdeci <moura97.valdeci@gmail.com>
2021-08-13 13:26:30 -0300

    Exercício 2

--- bloco4-JS-Log-prog/dia4.2-arrayFor-js/array.js
+++ bloco4-JS-Log-prog/dia4.2-arrayFor-js/array.js
@@ -1,6 +1,17 @@
 //Exercício 1
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let somatorio = 0;
 
 for(let items of numbers) {
-  console.log(items);
+  //console.log(items);
 }
+
+//Exercício 2
+function sigma(numbers){
+  for (let i = 0; i < numbers.length; i += 1) {
+    somatorio = somatorio + numbers[i];
+  }
+  return somatorio;
+}
+
+//console.log(sigma(numbers));
