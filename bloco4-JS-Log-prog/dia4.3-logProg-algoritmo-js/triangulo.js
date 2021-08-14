e50d1d0cf8c892a35bdccf53a1f541a3bbafb78a
Valdeci <moura97.valdeci@gmail.com>
2021-08-14 17:15:04 -0300

    Exercício 3

--- bloco4-JS-Log-prog/dia4.3-logProg-algoritmo-js/triangulo.js
+++ bloco4-JS-Log-prog/dia4.3-logProg-algoritmo-js/triangulo.js
@@ -1,6 +1,28 @@
+//Exercício 2
 let n = 5;
 let ast = '*';
 
 for (let i = 1; i <= n; i += 1) {
   console.log(ast.repeat(i));
 }
+
+//Exercício 3 (espelhar o ex. 2)
+let x = 5;
+let symbol = '*';
+let inputLine = '';
+let position = x;
+
+for (let iLine = 0; iLine < x; iLine += 1) {
+  for (let iColumn = 1; iColumn <= x; iColumn += 1) {
+    if (iColumn < position) {
+      inputLine = inputLine + ' ';
+    } else {
+      inputLine = inputLine + symbol;
+    }
+  }
+  console.log(inputLine);
+  inputLine = '';
+  position -= 1;
+}
+
+//Entendi o motivo de atribuir o x à outra variável. Esse era o meu erro no momento de atribuição das ordens dos for's.
