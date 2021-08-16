5d7bbed8f8da4ed1f8a91f3d5b476f4b8577460e
Valdeci <moura97.valdeci@gmail.com>
2021-08-15 21:32:39 -0300

    Exercicio 5

--- bloco4-JS-Log-prog/dia4.4-objetos-funcoes/objetos-forIn.js
+++ bloco4-JS-Log-prog/dia4.4-objetos-funcoes/objetos-forIn.js
@@ -4,17 +4,36 @@ let info = {
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald.'
 };
 
-console.log('Bem vinda, ' + info.personagem + '!');
-
-  info['recorrente'] = 'sim';
+//console.log('Bem vinda, ' + info.personagem + '!');
+//
+info['recorrente'] = 'sim';
 //Ou info.recorrente = 'sim';
+//
+//console.log(info);
+//
+//for(item in info){
+//  console.log(item);
+//};
+//
+//for(item in info){
+//  console.log(info[item]);
+//};
 
-console.log(info);
-
-for(item in info){
-  console.log(item);
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'sim'
 };
 
-for(item in info){
-  console.log(info[item]);
-};
+for (let properties in info) {
+  if (
+    properties === 'recorrente' &&
+    info[properties] === 'Sim' &&
+    info2[properties] === 'Sim'
+  ) {
+    console.log('Ambos recorrentes');
+  } else {
+    console.log(info[properties] + ' e ' + info2[properties]);
+  }
+}
