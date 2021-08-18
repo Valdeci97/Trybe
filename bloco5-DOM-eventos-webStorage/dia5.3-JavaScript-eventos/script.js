c8354726c64277ad0ab22ca18f9fbf228991a0f4
Valdeci <moura97.valdeci@gmail.com>
2021-08-18 12:13:30 -0300

    Exercício 5

--- bloco5-DOM-eventos-webStorage/dia5.3-JavaScript-eventos/script.js
+++ bloco5-DOM-eventos-webStorage/dia5.3-JavaScript-eventos/script.js
@@ -67,3 +67,32 @@ function changingHolidays(){
 }
 
 changingHolidays();
+
+function createButtonFriday(a){
+  let fridayButton = document.createElement('button');
+  let divButtonsNew = document.querySelector('.buttons-container');
+  fridayButton.innerHTML = a;
+  fridayButton.id = 'btn-friday';
+  divButtonsNew.appendChild(fridayButton);
+};
+
+createButtonFriday('Sexta-feira');
+
+function displayFridays(fridaysArray) {
+  let getFridayButton = document.querySelector('#btn-friday');
+  let fridays = document.getElementsByClassName('friday');
+  let newFridayText = 'SEXTOU o/';
+
+  getFridayButton.addEventListener('click', function() {
+  for (let index = 0; index < fridays.length; index += 1) {
+    if (fridays[index].innerHTML !== newFridayText) {
+        fridays[index].innerHTML = newFridayText;
+    } else {
+        fridays[index].innerHTML = fridaysArray[index];
+      }
+    }
+  })
+};
+
+let dezFridays = [ 4, 11, 18, 25 ];
+displayFridays(dezFridays);
