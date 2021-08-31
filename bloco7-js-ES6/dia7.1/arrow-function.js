0cc8029abc3d6460035d01f5ee167bf712f0514a
Valdeci <moura97.valdeci@gmail.com>
2021-08-31 13:04:22 -0300

    Exercícios 1 e 2 da parteII

--- bloco7-js-ES6/dia7.1/arrow-function.js
+++ bloco7-js-ES6/dia7.1/arrow-function.js
@@ -24,3 +24,41 @@ const arrayColection = (a, b) => (a > b);
 console.log(oddsAndEvens.sort(arrayColection));
 
 //Duas formas de se retornar o array numérico em ordem crescente. fonte: https://pt.stackoverflow.com/questions/9900/como-ordenar-um-array-por-valores
+
+//Parte II
+
+//exercício 1
+const factorial = (n) => {
+  let result = 1;
+  for (let i = 2; i <= n; i += 1) {
+    result *= i;
+  }
+  return result;
+}
+
+console.log(factorial(5));
+
+const factorialNumber = n => n > 1 ? n * factorialNumber(n - 1) : 1;
+
+console.log(factorialNumber(5));
+
+//Exercício 2
+const comparingWords = (x) => {
+  let words = x.split(' ');
+  let count = 0;
+  let text = '';
+  for (const word of words) {
+    if (word.length > count) {
+      count = word.length;
+      text = word;
+    }
+  }
+  return text;
+}
+
+console.log(comparingWords('Antônio foi ao banheiro e não sabemos o que aconteceu'));
+
+const longestWord = text => text.split(' ').sort((wordA, wordB) => wordB.length - wordA.length)[0]; //Separa o texto quando acha um espaço. organiza recebendo dois parâmetros, que vão ser aplicados em uma função.
+//Essa função retorna o comprimento desses parâmetros de forma crescente a partir da posição 1 (index === 0).
+
+console.log(longestWord("Antonio foi no banheiro e não sabemos o que aconteceu"));
