b756a9555739182c7964f86582025ca0020c13d4
Valdeci <moura97.valdeci@gmail.com>
2021-09-02 18:50:39 -0300

    Refatora ex.1 e faz ex.3 dia8.1

--- bloco8-HOF-ES6/dia8.1/HOF-tests.js
+++ bloco8-HOF-ES6/dia8.1/HOF-tests.js
@@ -12,55 +12,64 @@ doingThings(timeToSleep());
 
 // Exercício 1: Crie uma função que retorne um objeto no formato {nome completo, email} respresentando uma nova pessoa contratada.
 
-const firstID = (nome) => {
-  const person = nome.replace(' ', '_');
-  const info = {
-    name: nome,
-    email: `${person}@trybe.com`,
-  };
-  return info;
+const teamMate = (nome) => {
+  const email = `${nome.toLowerCase().replace(' ', '_')}@trybe.com` ;
+  return `${nome}, ${email}`;
 };
 
-const secondId = (nome) => {
-  const person = nome.replace(' ', '_');
-  const info = {
-    name: nome,
-    email: `${person}@trybe.com`,
-  };
-  return info;
-};
-
-const thirdId = (nome) => {
-  const person = nome.replace(' ', '_');
-  const info = {
-    name: nome,
-    email: `${person}@trybe.com`,
-  };
-  return info;
-};
-
-const newEmployees = (object1, object2, object3) => {
+const newEmployees = (object) => {
   const employees = {
-    id1: object1,
-    id2: object2,
-    id3: object3,
+    id1: object('Pedro Guerra'),
+    id2: object('Luiza Drumond'),
+    id3: object('Carla Paiva')
   }
   return employees;
 };
 
-console.log(newEmployees(firstID('Pedro Guerra'), secondId('Luiza Drumond'), thirdId('Carla Paiva')));
+console.log(newEmployees(teamMate));
 
 // Exercício 2: Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
 
 const randomNumber = () => Math.round(Math.random() * 5);
 
 const goodLuck = (number, func) => {
-  if (number === func) {
-    console.log(`O número escolhido foi ${number} e o sorteado foi ${func}.`);
+  x = func()
+  if (number === x) {
+    console.log(`O número escolhido foi ${number} e o sorteado foi ${x}.`);
     return 'Parabéns, você ganhou!';
   }
-  console.log(`O número escolhido foi ${number} e o sorteado ${func}.`);
+  console.log(`O número escolhido foi ${number} e o sorteado ${x}.`);
   return 'Tente novamente!';
 };
 
-console.log(goodLuck(2, randomNumber()));
+console.log(goodLuck(2, randomNumber));
+
+// Exercício 3: Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
+// Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A") não altera-se a contagem.
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const testAnalyser = (x, y) => {
+  let count = 0;
+  if (y.length === 0){
+    return count;
+  }
+  for(let i = 0; i < x.length; i += 1) {
+    if (y[i] === 'N.A') {
+      count = count;
+    } else if(y[i] === x[i]) {
+      count += 1;
+    } else {
+      count -= 0.5;
+    };
+  } 
+  return count;
+}
+
+
+const testResult = (x, y, func) => {
+  console.log(func(x, y));
+};
+
+testResult(RIGHT_ANSWERS, STUDENT_ANSWERS, testAnalyser);
