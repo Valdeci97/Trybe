6adb3538e3537d4c376bb5c53bc254605d5d0fca
Valdeci <moura97.valdeci@gmail.com>
2021-09-02 16:49:35 -0300

    Exercício 2 do dia 8.1

--- bloco8-HOF-ES6/dia8.1/HOF-tests.js
+++ bloco8-HOF-ES6/dia8.1/HOF-tests.js
@@ -52,3 +52,15 @@ console.log(newEmployees(firstID('Pedro Guerra'), secondId('Luiza Drumond'), thi
 
 // Exercício 2: Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
 
+const randomNumber = () => Math.round(Math.random() * 5);
+
+const goodLuck = (number, func) => {
+  if (number === func) {
+    console.log(`O número escolhido foi ${number} e o sorteado foi ${func}.`);
+    return 'Parabéns, você ganhou!';
+  }
+  console.log(`O número escolhido foi ${number} e o sorteado ${func}.`);
+  return 'Tente novamente!';
+};
+
+console.log(goodLuck(2, randomNumber()));
