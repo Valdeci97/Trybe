d346d5be2a86d4af2c39ada2a9f0bb513e6fa5b1
Valdeci <moura97.valdeci@gmail.com>
2021-09-03 21:29:01 -0300

    Inicia dia 8.2

---
+++ bloco8-HOF-ES6/dia8.2/aula.js
@@ -0,0 +1,84 @@
+// const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// const multipliesFor2 = (element) => {
+//   console.log(`${element} * 2: ${element * 2}`);
+// };
+
+// numbers.forEach(multipliesFor2);
+
+// const names = ['Bianca', 'Camila', 'Fernando', 'Ana Roberta'];
+
+// const convertToUpperCase = (name, index) => {
+//   names[index] = name.toUpperCase();
+// };
+
+// names.forEach(convertToUpperCase);
+// console.log(names);
+
+// // Para fixar
+
+// const emailListInData = [
+//   'roberta@email.com',
+//   'paulo@email.com',
+//   'anaroberta@email.com',
+//   'fabiano@email.com',
+// ];
+
+// const showEmailList = (email) => {
+//   console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+// };
+
+// emailListInData.forEach(showEmailList);
+
+// //
+
+// const number = [19, 21, 30, 3, 45, 22, 15];
+
+// const verifyEven = (number) => number % 2 === 0;
+
+// const isEven = number.find(verifyEven);
+
+// console.log(isEven); // 30
+
+//console.log(verifyEven(9)); // False
+//console.log(verifyEven(14)); // True
+
+// Outra forma de ser realizada sem a necessidade de criar uma nova função.
+//const isEven2 = number.find((number) => number % 2 === 0);
+
+//console.log(isEven2); // 30
+
+// Para fixar 2
+
+// const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+// const findDivisibleBy3And5 = (number) => {
+//   if (number % 3 === 0 && number % 5 === 0) {
+//     return number;
+//   }
+// }
+
+// console.log(numbers.find((number) => findDivisibleBy3And5(number)));
+
+// const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+// const findNameWithFiveLetters = (items) => {
+//   if (items.length === 5) {
+//     return items;
+//   }
+// }
+
+// console.log(names.find(findNameWithFiveLetters)); // Estava fazendo a lógica invertida :/
+
+// const musicas = [
+//   { id: '31031685', title: 'Partita in C moll BWV 997' },
+//   { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+//   { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+// ]
+
+// const findMusic = (id) => {
+//   return musicas.find((musica) => musica.id === id);
+// };
+
+// console.log(findMusic('31031685'));
+
