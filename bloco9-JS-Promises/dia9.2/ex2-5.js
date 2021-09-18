20854ba64113fe701245bdbec5f5b8496d98a25e
Valdeci <moura97.valdeci@gmail.com>
2021-09-18 00:09:41 -0300

    Exercício 2 ao 5 do dia 9.2 + coment ex bônus

--- bloco9-JS-Promises/dia9.2/ex2-5.js
+++ bloco9-JS-Promises/dia9.2/ex2-5.js
@@ -9,18 +9,60 @@
 
 const fecthPromise = () => {
   const myPromise = new Promise ((resolve, reject) => {
-    const myArray = array.from({ length: 10 },
-       () => Math.floor((Math.random() * 50) + 1));
+    const myArray = Array.from({ length: 10 }, () => Math.floor((Math.random() * 50) + 1));
 
-       const sum = myArray.map((element) => element * element)
-       .reduce((sum, number) => sum + number);
+    const sum = myArray.map((element) => element * element).reduce(((sum, number) => sum + number), 0);
 
-       (sum < 8000) ? resolve() : reject();
+    sum < 8000 ? resolve(sum) : reject(sum);
   });
 
-  myPromise
-  .then(() => console.log('Promise Resolvida'))
-  .catch(() => console.log('Promise rejeitada'));
+  // myPromise
+  // .then(() => console.log('Promise Resolvida'))
+  // .catch(() => console.log('Promise rejeitada'));  Exercício 2
+
+  // myPromise
+  //   .then((sum) => [2, 3, 5, 10].map(element => sum / element))
+  //   .then((sum) => console.log(sum))
+  //   .catch((sum) => console.log(`Promise rejeitada ${sum}`));  Exercício 3
+
+  // myPromise
+  // .then((sum) => [2, 3, 5, 10].map((element) => element / sum))
+  // .then((result) => console.log(result))
+  // .catch(() => console.log(`É mais de oito mil! Essa promise deve estar quebrada!`)); exercício 4
+
+  myPromise.then((sum) => [2, 3, 5, 10].map((element) => sum / element))
+  .then((result) => result.reduce(((acc, curr) => acc + curr), 0))
+  .then((number) => console.log(number.toPrecision(2)))
+  .catch(() => console.log(`É mais de oito mil! Essa promise deve estar quebrada!`));
 }
 
 fecthPromise();
+
+// const sumRandomNumbers = () => { Exercício bônus tudo com await e async, try e catch.
+//   const myArray = Array.from(
+//     { length: 10 },
+//     () => Math.floor(Math.random() * 50) + 1
+//   );
+//   const sum = myArray.map(number => number * number)
+//     .reduce((number, acc) => number + acc, 0);
+
+//   if (sum >= 8000) {
+//     throw new Error();
+//   }
+
+//   return sum;
+// }
+
+// const sumArrayFromSum = (sum) => [2, 3, 5, 10].map(number => sum / number)
+//   .reduce((number, acc) => number + acc);
+
+// const fetchPromise = async () => {
+//   try {
+//     const sum = await sumRandomNumbers();
+//     const sumFromSum = await sumArrayFromSum(sum);
+//   } catch (error) {
+//     console.log('É mais de oito mil! Essa promise deve estar quebrada!');
+//   }
+// }
+
+// fetchPromise();
