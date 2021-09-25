dd728598be136ce7b3e0c4bc45f5b98475d9ba57
Valdeci <moura97.valdeci@gmail.com>
2021-09-25 16:59:46 -0300

    Exercícios 2 e 3 feitos

--- bloco10-jest/dia10.3/NumberByNumber.test.js
+++ bloco10-jest/dia10.3/NumberByNumber.test.js
@@ -1,10 +1,32 @@
 const object = require('./numberByNumber');
 
-test('Analisa se a função ao ser implementada para receber dois números retorna sua divissão', () => {
-  object.randomNumber = jest.fn().mockImplementationOnce((a, b) => a / b);
+// test('Analisa se a função ao ser implementada para receber dois números retorna sua divissão', () => {
+//   object.randomNumber = jest.fn().mockImplementationOnce((a, b) => a / b);
 
-  expect(object.randomNumber(10, 2)).toBe(5);
-  expect(object.randomNumber).toHaveBeenCalled()
+//   expect(object.randomNumber(10, 2)).toBe(5);
+//   expect(object.randomNumber).toHaveBeenCalled()
+//   expect(object.randomNumber).toHaveBeenCalledTimes(1);
+//   expect(object.randomNumber).toHaveBeenCalledWith(10, 2);
+// }); Exercício 2
+
+test('Analisa se a função ao ser implementada para receber três parâmetro retornar a multiplicação',
+() => {
+  object.randomNumber = jest.fn().mockImplementation((a, b, c) => a * b * c);
+
+  expect(object.randomNumber(3, 3, 3)).toBe(27);
+  expect(object.randomNumber).toHaveBeenCalled();
   expect(object.randomNumber).toHaveBeenCalledTimes(1);
-  expect(object.randomNumber).toHaveBeenCalledWith(10, 2);
+  expect(object.randomNumber).toHaveBeenCalledWith(3, 3, 3);
+})
+
+test('Analise se a função ao ser implementada retorna o dobro do parâmetro recebido', () => {
+  object.randomNumber.mockReset();
+  expect(object.randomNumber).toHaveBeenCalledTimes(0);
+
+  object.randomNumber.mockImplementation((a) => a * 2);
+
+  expect(object.randomNumber(5)).toBe(10);
+  expect(object.randomNumber).toHaveBeenCalled();
+  expect(object.randomNumber).toHaveBeenCalledTimes(1);
+  expect(object.randomNumber).toHaveBeenCalledWith(5);
 });
