f3a2cbe8f1bc9e5aa441b2f26f4292c78716b96e
Valdeci <moura97.valdeci@gmail.com>
2021-09-29 20:29:00 -0300

    exercícios da aula

---
+++ Front-End/dia11.1/ex-Aula/ex-dia11.1/JSX.js
@@ -0,0 +1,25 @@
+const textJSX = 'Hello, JSX';
+const element = <h1>textJSX</h1>;
+
+
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+//   ReactDOM.render(element, document.getElementById('root'));
+// }
+
+// setInterval(tick, 1000); exemplo de interação com o DOM advindo da aula.
+
+// import React from 'react';
+
+// class ReactClass extends React.Component {
+//   render() {
+//     return (
+//       <h1>My first React Class Component!</h1>
+//     )
+//   }
+// } Exemplo de classe com react
---
+++ Front-End/dia11.1/ex-Aula/ex-dia11.1/hello-world-test/src/Component.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class HelloWorld extends React.Component {
+  render() {
+    return (<><h1>Valdeci de Moura</h1><p>Um moleque curioso que está se aventurando nesse mundo de programação.</p></>)
+  };
+};
+
+export default HelloWorld;
