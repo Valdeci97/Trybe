a140d00832c86d3647577822ca55f8d21428a2ac
Valdeci <moura97.valdeci@gmail.com>
2021-09-30 16:33:05 -0300

    Exercícios restantes do dia11.1

---
+++ Front-End/dia11.1/exercicios/exercises-part-ii/exercises-react/src/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+    <>
+    <footer className="footer">
+      <h1>E isso é só o começo</h1>
+    </footer>
+    </>)
+  }
+}
+
+export default Footer;
+
+// conteúdo quando entre chaves {} deve ser tratado, exemplo strings também precisam de apóstrofos ou faz-se necessário o uso de template literals.
