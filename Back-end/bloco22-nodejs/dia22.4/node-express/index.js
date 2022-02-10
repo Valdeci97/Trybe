f962bad9a07f823dbc508db98d20531480b031f7
Valdeci <moura97.valdeci@gmail.com>
2022-02-10 13:49:58 -0300

    Exercício 1

---
+++ Back-end/bloco22-nodejs/dia22.4/node-express/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/ping', (_req, res) => res.json({ "message": "pong" }));
+
+app.use(function (err, req, res, next) {
+  res.status(500).send(`Algo deu errado! Mensagem: ${err.message}`);
+});
+
+app.listen(3000, () => console.log('ouvindo na porta 3000!'));
