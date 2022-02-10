c45f2741564b33a3736664a740d048fc030b8480
Valdeci <moura97.valdeci@gmail.com>
2022-02-10 14:14:35 -0300

    exercícios 2 e 3

--- Back-end/bloco22-nodejs/dia22.4/node-express/index.js
+++ Back-end/bloco22-nodejs/dia22.4/node-express/index.js
@@ -7,6 +7,20 @@ app.use(bodyParser.json());
 
 app.get('/ping', (_req, res) => res.json({ "message": "pong" }));
 
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  res.status(200).json({ "message": `Hello, ${name}` });
+});
+
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
+  if (age < 17) {
+    res.status(401).json({ "message": "unauthorized" });
+  }
+
+  res.status(200).json({ "message": `Hello, ${name}` });
+});
+
 app.use(function (err, req, res, next) {
   res.status(500).send(`Algo deu errado! Mensagem: ${err.message}`);
 });
