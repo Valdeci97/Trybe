7fbd7e66aa2a193a87ed5a841638cf86d3fda209
Valdeci <moura97.valdeci@gmail.com>
2022-02-11 18:47:40 -0300

    exercício 4

--- Back-end/bloco22-nodejs/dia22.5/middlewares/index.js
+++ Back-end/bloco22-nodejs/dia22.5/middlewares/index.js
@@ -9,6 +9,12 @@ const {
 const btcToken = require('./helpers/btcToken');
 
 const BTC_API = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json';
+const posts = [
+  { id: "1", author: "João Cancelo", comment: "Tarde boa!" },
+  { id: "2", author: "Julia Neves", comment: "Tarde cheia!" },
+  { id: "3", author: "Natália Scheneider", comment: "Cara, que calor..." },
+  { id: "4", author: "Ryan Silva Matos", comment: "La casa de papel tá demais" }
+];
 
 const app = express();
 
@@ -28,6 +34,13 @@ app.post('/user/login', emailValidator, passwordValidator, (_req, res) => {
 app.get('/btc/price', btcToken, async (_req, res) => {
   const response = await axios.get(BTC_API);
   res.status(200).json(response.data);
+});
+
+app.get('/posts/:id', (req, res) => {
+  const { id } = req.params;
+  const post = posts.find((post) => post.id === id);
+  if (!post) res.status(404).send({ message: 'Post Not found' });
+  return res.status(200).send(post);
 });
 
 app.listen(3000, () => console.log('Ouvindo na porta 3000.'));
