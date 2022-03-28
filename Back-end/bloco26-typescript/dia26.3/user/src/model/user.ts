7be6fa7276fa5c7250b630c9382132b3397d7c72
Valdeci97 <moura97.valdeci@gmail.com>
2022-03-28 10:44:01 -0300

    New:

--- Back-end/bloco26-typescript/dia26.3/user/src/model/user.ts
+++ Back-end/bloco26-typescript/dia26.3/user/src/model/user.ts
@@ -1,5 +1,25 @@
-type user = {
+import { Pool } from 'mysql2/promise';
+
+export type user = {
   nome: string;
   email: string;
   senha: string;
 };
+
+interface IUser {
+  findAll: () => user[];
+  findOne: () => user;
+  update: () => user;
+  create: () => user;
+  destroy: () => void;
+};
+
+export class User implements IUser {
+  connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  };
+
+  findAll = async () => await this.connection.execute();
+}
